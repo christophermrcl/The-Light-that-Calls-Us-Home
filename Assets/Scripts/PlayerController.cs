b9cba1174a1f9b9f89efe0382f7327f1e6f27ab7
Christopher Marcellionel Young <cmarcellionel@gmail.com>
2024-10-07 11:43:31 +0700

    Commit 7.10.2024a
    - Input system now use input action
    - Added ranged attack system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,69 +1,179 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController1 : MonoBehaviour
 {
-    public float speed;
-    public float groundDist;
-
-    public LayerMask terrainLayer;
     public Rigidbody rb;
     public SpriteRenderer sr;
     public Animator anim;
-    public PlayerState pst;
 
-    public float jumpForce = 20f;
+    public float speed;
+    public float jumpSpeed;
+    public float jumpButtonGracePeriod;
+
+    private CharacterController characterController;
+    private Vector2 moveDirection;
+    private float ySpeed;
+    private float originalStepOffset;
+    private float? lastGroundedTime;
+    private float? jumpButtonPressedTime;
+
+    private bool isToolUse = false;
+    private int comboCount = 0;
+    private float lastClickTime;
+    public GameObject RangedPrefab;
+
+
+    public GameObject trajectoryLine;
+    private Vector2 mousePosition;
+    private Vector2 screenCenter;
+    private Vector2 trajectoryDirection;
+    private float rangedAngle;
+
+    public GameObject meleeCollider;
+    public PlayerInputAction playerControls;
+    private InputAction move;
+    private InputAction jump;
+    private InputAction attack;
+    private InputAction rangedGamepad;
+    private InputAction rangedMouse;
+    private InputAction aimGamepad;
+
     // Start is called before the first frame update
+
+    private void Awake()
+    {
+        playerControls = new PlayerInputAction();
+    }
     void Start()
     {
-        pst = gameObject.GetComponent<PlayerState>();
+        characterController = gameObject.GetComponent<CharacterController>();
         rb = gameObject.GetComponent<Rigidbody>();
         sr = gameObject.GetComponent<SpriteRenderer>();
         anim = gameObject.GetComponent<Animator>();
+
+
+        originalStepOffset = characterController.stepOffset;
+    }
+
+    private void OnEnable()
+    {
+        move = playerControls.Control.Move;
+        move.Enable();
+
+        jump = playerControls.Control.Jump;
+        jump.Enable();
+        jump.performed += Jump;
+
+        attack = playerControls.Control.Attack;
+        attack.Enable();
+        attack.performed += Attack;
+
+        rangedGamepad = playerControls.Control.RangedGamepad;
+        rangedGamepad.Enable();
+        rangedMouse = playerControls.Control.RangedMouse;
+        rangedMouse.Enable();
+
+        aimGamepad = playerControls.Control.AimGamepad;
+        aimGamepad.Enable();
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+        jump.Disable();
+        attack.Disable();
+        rangedGamepad.Disable();
+        rangedMouse.Disable();
+        aimGamepad.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
-        RaycastHit hit;
-        Vector3 castPos = transform.position;
-        castPos.y += 1;
+        if (rangedGamepad.IsPressed() || rangedMouse.IsPressed())
+        {
+            trajectoryLine.SetActive(true);
+            TrajectoryLine();
+        }
+        else
+        {
+            trajectoryLine.SetActive(false);
+        }
+        
+        
+        AttackAnimCheck();
+
+        CharacterMove();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void CharacterMove()
+    {
+        moveDirection = move.ReadValue<Vector2>();
+
+        float horizontalInput = moveDirection.x;
+        float verticalInput = moveDirection.y;
+
+        if (isToolUse)
+        {
+            horizontalInput = 0;
+            verticalInput = 0;
+        }
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        float magnitude = Mathf.Clamp01(movementDirection.magnitude) * speed;
+        movementDirection.Normalize();
+
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+
+        if (characterController.isGrounded)
+        {
+            lastGroundedTime = Time.time;
+        }
 
         
-        if (Physics.Raycast(castPos, -transform.up, out hit, Mathf.Infinity, terrainLayer))
+
+        if (Time.time - lastGroundedTime <= jumpButtonGracePeriod)
         {
-            if (hit.collider != null)
+            characterController.stepOffset = originalStepOffset;
+            ySpeed = -0.5f;
+
+            if (Time.time - jumpButtonPressedTime <= jumpButtonGracePeriod)
             {
-                Vector3 movePos = transform.position;
-                movePos.y = hit.point.y + groundDist;
-                transform.position = movePos;
+                anim.SetBool("isJumping", true);
+                ySpeed = jumpSpeed;
+                jumpButtonPressedTime = null;
+                lastGroundedTime = null;
             }
         }
-        */
-
-        //MOVE
-
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-
-        Vector3 moveDir = new Vector3(x, 0, y);
-        rb.velocity = moveDir * speed;
-
-        //JUMP
-
-        if (pst.isGrounded && Input.GetButtonDown("Jump"))
+        else
         {
-            pst.isJumping = true;
-            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+            characterController.stepOffset = 0;
         }
+
+        Vector3 velocity = movementDirection * magnitude;
+        velocity.y = ySpeed;
+
+
+        characterController.Move(velocity * Time.deltaTime);
+        
+        float x = horizontalInput;
+        float y = verticalInput;
 
         if (x != 0 && x < 0)
         {
             sr.flipX = false;
 
-        } else if (x != 0 && x > 0)
+        }
+        else if (x != 0 && x > 0)
         {
             sr.flipX = true;
         }
@@ -80,14 +190,118 @@ public class PlayerController : MonoBehaviour
         if (y != 0 && y > 0)
         {
             anim.SetBool("faceFront", false);
-        }else if(y != 0 && y < 0)
+        }
+        else if (y != 0 && y < 0)
         {
             anim.SetBool("faceFront", true);
         }
+
+        if (characterController.isGrounded)
+        {
+            anim.SetBool("isJumping", false);
+        }
+
+        MeleeDirection(x);
     }
 
-    private void FixedUpdate()
+    private void Jump(InputAction.CallbackContext context) 
+    {
+        if (!isToolUse && Input.GetButtonDown("Jump"))
+        {
+            jumpButtonPressedTime = Time.time;
+        }
+    }
+
+    private void Attack(InputAction.CallbackContext context) 
+    {
+        if (!isToolUse && !(rangedGamepad.IsPressed() || rangedMouse.IsPressed()))
+        {
+            MeleeAttack();
+        }else if (!isToolUse && (rangedGamepad.IsPressed() || rangedMouse.IsPressed()))
+        {
+            RangedAttack();
+        }
+    }
+
+    private void MeleeAttack()
+    {
+            if(comboCount <= 0)
+            {
+                comboCount++;
+                isToolUse = true;
+                anim.SetInteger("attackSeq", comboCount);
+                anim.SetBool("isAttacking", true);
+                lastClickTime = Time.time;
+            }
+    }
+
+    private void RangedAttack()
+    {
+        GameObject projectile = Instantiate(RangedPrefab, transform.position, Quaternion.identity);
+
+        // Set the rotation of the projectile
+        projectile.transform.rotation = Quaternion.Euler(0, -rangedAngle, 0);
+
+        // Get the direction vector from the angle
+        Vector3 direction = new Vector3(Mathf.Cos(rangedAngle * Mathf.Deg2Rad), 0, Mathf.Sin(rangedAngle * Mathf.Deg2Rad));
+
+        // Add force or velocity to the projectile to move it in the direction
+        Rigidbody rb = projectile.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.velocity = direction * 5f;
+        }
+    }
+
+    private void AttackAnimCheck()
     {
         
+        if (!isToolUse && Time.time - lastClickTime > 0.5f)
+        {
+            comboCount = 0;
+        }
+        if (anim.GetBool("isAttacking") && anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.3f)
+        {
+            meleeCollider.SetActive(true);
+        }
+        if (anim.GetBool("isAttacking") && anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.4f)
+        {
+            meleeCollider.SetActive(false);
+        }
+        if (anim.GetBool("isAttacking") && anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
+        {
+            isToolUse = false;
+            anim.SetBool("isAttacking", false);
+        }
+    }
+
+    private void MeleeDirection(float x)
+    {
+        if(x > 0)
+        {
+            meleeCollider.transform.localScale = new Vector3(-1f, meleeCollider.transform.localScale.y, meleeCollider.transform.localScale.z);
+        }else if(x < 0)
+        {
+            meleeCollider.transform.localScale = new Vector3(1f, meleeCollider.transform.localScale.y, meleeCollider.transform.localScale.z);
+        }
+    }
+
+    private void TrajectoryLine()
+    {
+        if (rangedMouse.IsPressed())
+        {
+            mousePosition = Input.mousePosition;
+            screenCenter = new Vector2(Screen.width / 2, Screen.height / 2);
+            trajectoryDirection = mousePosition - screenCenter;
+        }
+        else if (rangedGamepad.IsPressed())
+        {
+            trajectoryDirection = aimGamepad.ReadValue<Vector2>();
+        }
+        
+
+        rangedAngle = Mathf.Atan2(trajectoryDirection.y, trajectoryDirection.x) * Mathf.Rad2Deg;
+
+        trajectoryLine.transform.rotation = Quaternion.Euler(0, -rangedAngle, 0);
     }
 }
